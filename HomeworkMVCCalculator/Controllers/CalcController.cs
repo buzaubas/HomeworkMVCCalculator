aa4b9713eef66d66d51f63a978904d56ee1d9ffd
БексултановД <beks.dimash@mail.ru>
2023-05-15 19:51:11 +0600

    Добавьте файлы проекта.

---
+++ HomeworkMVCCalculator/Controllers/CalcController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkMVCCalculator.Controllers
+{
+    public class CalcController : Controller
+    {
+          public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[controller]/Add/{num1:int}/{num2:int}")]
+        public IActionResult Add(int num1 = 0, int num2 = 0)
+        {
+            ViewBag.Result = (num1 + num2).ToString();
+            return View();
+        }
+
+        [Route("[controller]/Mul/{num1:int}/{num2:int}")]
+        public IActionResult Mul(int num1 = 0, int num2 = 0)
+        {
+            ViewBag.Result = (num1 * num2).ToString();
+            return View();
+        }
+
+        [Route("[controller]/Div/{num1:int}/{num2:int}")]
+        public IActionResult Div(int num1 = 0, int num2 = 1)
+        {
+            ViewBag.Result = (num1 / num2).ToString();
+            return View();
+        }
+
+        [Route("[controller]/Sub/{num1:int}/{num2:int}")]
+        public IActionResult Sub(int num1 = 0, int num2 = 0)
+        {
+            ViewBag.Result = (num1 - num2).ToString();
+            return View();
+        }
+    }
+}
---
+++ HomeworkMVCCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeworkMVCCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
